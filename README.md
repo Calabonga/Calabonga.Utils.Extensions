00e813b59f62705670c4d14ca07403f469480f0a
Sergei Calabonga <dev@calabonga.net>
2025-01-19 16:53:49 +0700

    added new WithIndex

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Calabonga.Utils.Extensions
 Some helpful extensions for C#.NET. Package contains extensions (C#) that can simplify developer life.
+
+## Change History
+
+### 1.0.1
+
+* `WithIndex` method added to `EnumerableExtensions`
---
+++ src/Calabonga.Utils.Extensions/EnumerableExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Calabonga.Utils.Extensions
+{
+    /// <summary>
+    /// Enum extensions
+    /// </summary>
+    public static class EnumerableExtensions
+    {
+        /// <summary>
+        /// Enumerate with Index
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        [DebuggerStepThrough]
+        public static IEnumerable<(T item, int index)> WithIndex<T>(this IEnumerable<T> source)
+                => source.Select((item, index) => (item, index));
+
+    }
+}
